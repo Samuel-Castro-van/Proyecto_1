b2383339583961dcdcc51461c57ac2e94b447813
samuel <samuel.castro.van27@gmail.com>
2026-07-01 18:58:21 -0500

    Implementación de modelos delectura y correcion de errores.

--- app/enrutadores/transacciones.py
+++ app/enrutadores/transacciones.py
@@ -1,27 +1,25 @@
 from fastapi import APIRouter, HTTPException, status, Depends
 from typing import List
 from sqlmodel import Session, select
-from app.modelos.transacciones import Transaccion, TransaccionCrear, TransaccionEditar
+from app.modelos.transacciones import Transaccion, TransaccionCrear, TransaccionEditar, TransaccionLeer
 from app.modelos.facturas import Factura
 from app.database import get_session
 
 rutas_transacciones = APIRouter()
 
-# --- ENDPOINTS TRANSACCIONES ---
 
-@rutas_transacciones.get("/transacciones", response_model=List[Transaccion])
+@rutas_transacciones.get("/transacciones", response_model=List[TransaccionLeer])
 def listar_transacciones(session: Session = Depends(get_session)):
     transacciones = session.exec(select(Transaccion)).all()
     return transacciones
 
 @rutas_transacciones.post("/transacciones/{factura_id}", response_model=Transaccion)
 def crear_transaccion(factura_id: int, datos_transaccion: TransaccionCrear, session: Session = Depends(get_session)):
-    # 1. Verificamos que la factura exista
+
     factura = session.get(Factura, factura_id)
     if not factura:
         raise HTTPException(status_code=404, detail="Factura no encontrada")
     
-    # 2. Creamos la transacción vinculada a la factura
     nueva_transaccion = Transaccion.model_validate(datos_transaccion.model_dump())
     nueva_transaccion.factura_id = factura_id
     
@@ -36,7 +34,6 @@ def editar_transaccion(transaccion_id: int, datos_transaccion: TransaccionEditar
     if not transaccion:
         raise HTTPException(status_code=404, detail="Transacción no encontrada")
     
-    # Usamos sqlmodel_update para un código más profesional
     datos_dict = datos_transaccion.model_dump(exclude_unset=True)
     transaccion.sqlmodel_update(datos_dict)
         
